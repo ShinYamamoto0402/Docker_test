942817e3b1224345fb1e6eace1ea1cd6f1110ecd
ShinYamamoto0402 <aiue646802@icloud.com>
2023-07-28 14:19:57 +0900

    デザインの追加

--- index.php
+++ index.php
@@ -1,44 +1,19 @@
 <!DOCTYPE html>
-<html lang="ja">
+<html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>かわいいホームページ</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <title>Document</title>
 </head>
+
 <body>
-
-<!-- ナビゲーションバー -->
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">Cute Website</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-            <li class="nav-item active">
-                <a class="nav-link" href="#">ホーム</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">サービス</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">お問い合わせ</a>
-            </li>
-        </ul>
-    </div>
-</nav>
-
-<!-- メインコンテンツ -->
-<div class="container mt-4">
-    <h1>かわいいホームページへようこそ！</h1>
-    <p>ここに素敵なコンテンツが入ります。</p>
-</div>
-
-<!-- Bootstrapのスクリプトリンク -->
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-
+    <a href="https://care-con.co.jp/"></a>
+    <?php
+    for ($i = 0; $i <= 10; $i++) {
+        echo $i . "</br>";
+    }
+    ?>
 </body>
+
 </html>
